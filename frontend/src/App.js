235394439598122c6e5fc15decfc9880d93a9ee5
Abishek <abishekgasckcs@gmail.com>
2026-03-14 00:00:18 +0530

    fix admin unuser

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,6 @@ import ProductDetail from './pages/ProductDetail';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Cart from './pages/Cart';
-import Admin from './pages/Admin';
 import AdminDashboard from './pages/AdminDashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -21,29 +20,38 @@ function App() {
       <CartProvider>
         <div className="App">
           <Router>
-            <div>
-              <ToastContainer theme='dark' position='top-center' />
-              <Header />
-              <Routes>
-                  <Route path="/"  element={ <Home />}/>
-                  <Route path="/search"  element={ <Home />}/>
-                  <Route path="/product/:id"  element={ <ProductDetail />}/>
-                  <Route path="/cart"  element={ 
-                    <ProtectedRoute>
-                      <Cart />
-                    </ProtectedRoute>
-                  }/>
-                  <Route path="/admin"  element={ 
-                    <ProtectedRoute>
-                      <AdminDashboard />
-                    </ProtectedRoute>
-                  }/>
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
-              </Routes>
-            </div>
+            <ToastContainer theme="dark" position="top-center" />
+            <Header />
+
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/search" element={<Home />} />
+              <Route path="/product/:id" element={<ProductDetail />} />
+
+              <Route
+                path="/cart"
+                element={
+                  <ProtectedRoute>
+                    <Cart />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="/admin"
+                element={
+                  <ProtectedRoute>
+                    <AdminDashboard />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+
+            <Footer />
           </Router>
-          <Footer/>
         </div>
       </CartProvider>
     </AuthProvider>
